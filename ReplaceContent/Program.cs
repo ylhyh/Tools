c18c8749a8fa13eee28c6a53b0818a528fb6e6c1
ylhyh <ylhuyh@163.com>
2015-10-20 18:38:59 +0800

    Create init ver for Replace Content

---
+++ ReplaceContent/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReplaceContent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null || args.Length != 3)
+            {
+                Console.WriteLine("替换文本文件中的内容并写回源文件。内存替换，文件不要太大哦。");
+                Console.WriteLine();
+                Console.WriteLine("用法:");
+                Console.WriteLine("ReplaceContent [drive:][path]filename oldValue newValue");
+                Console.WriteLine();
+                Console.WriteLine("	[drive:][path]filename	指定要被替换的文件。");
+                Console.WriteLine("	oldValue		要被替换的字符串。");
+                Console.WriteLine("	newValue		要替换出现的所有 oldValue 的字符串。");
+                return;
+            }
+
+            try
+            {
+                using (FileStream fileStream = File.OpenRead(args[0]))
+                {
+                    Encoding fileEncoding = GetEncoding(fileStream);
+
+                    byte[] buffer = new byte[fileStream.Length];
+                    fileStream.Read(buffer, 0, (int)fileStream.Length);
+                    string fileContent=fileEncoding.GetString(buffer);
+                    fileStream.Close();
+
+                    string oldValue = args[1];
+                    string newValue = args[2];
+
+                    fileContent = fileContent.Replace(args[1], args[2]);
+
+                    byte[] outbuffer = fileEncoding.GetBytes(fileContent);
+                    FileStream writer = File.Open(args[0], FileMode.Truncate, FileAccess.Write, FileShare.Write);
+                    writer.Write(outbuffer, 0, outbuffer.Length);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        /// <summary>
+        /// 取得一个文本文件的编码方式。如果无法在文件头部找到有效的前导符，Encoding.Default将被返回。
+        /// </summary>
+        /// <param name="fileName">文件名</param>
+        /// 
+        public static Encoding GetEncoding(string fileName)
+        {
+            return GetEncoding(fileName, Encoding.Default);
+        }
+
+        /// <summary>
+        /// 取得一个文本文件流的编码方式。
+        /// </summary>
+        /// <param name="stream">文本文件流</param>
+        /// <returns></returns>
+        /// 
+        public static Encoding GetEncoding(FileStream stream)
+        {
+            return GetEncoding(stream, Encoding.Default);
+        }
+
+        /// <summary>
+        /// 取得一个文本文件的编码方式。
+        /// </summary>
+        /// <param name="fileName">文件名。</param>
+        /// <param name="defaultEncoding">默认编码方式。当该方法无法从文件的头部取得有效的前导符时，将返回该编码方式。</param>
+        /// <returns></returns>
+        /// 
+        public static Encoding GetEncoding(string fileName, Encoding defaultEncoding)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            Encoding targetEncoding = GetEncoding(fs, defaultEncoding);
+            fs.Close();
+            return targetEncoding;
+        }
+
+        /// <summary>
+        /// 取得一个文本文件流的编码方式。
+
+        /// </summary>
+        /// <param name="stream">文本文件流。</param>
+
+        /// <param name="defaultEncoding">默认编码方式。当该方法无法从文件的头部取得有效的前导符时，将返回该编码方式。</param>
+        /// <returns></returns>
+        /// 
+        public static Encoding GetEncoding(FileStream stream, Encoding defaultEncoding)
+        {
+            Encoding targetEncoding = defaultEncoding;
+            if (stream != null && stream.Length >= 2)
+            {
+                //保存文件流的前4个字节
+                byte byte1 = 0;
+                byte byte2 = 0;
+                byte byte3 = 0;
+                byte byte4 = 0;
+
+                //保存当前Seek位置
+                long origPos = stream.Seek(0, SeekOrigin.Begin);
+                stream.Seek(0, SeekOrigin.Begin);
+                int nByte = stream.ReadByte();
+                byte1 = Convert.ToByte(nByte);
+                byte2 = Convert.ToByte(stream.ReadByte());
+
+                if (stream.Length >= 3)
+                {
+                    byte3 = Convert.ToByte(stream.ReadByte());
+                }
+
+                if (stream.Length >= 4)
+                {
+                    byte4 = Convert.ToByte(stream.ReadByte());
+                }
+                //根据文件流的前4个字节判断Encoding
+                //Unicode {0xFF, 0xFE};
+                //BE-Unicode {0xFE, 0xFF};
+                //UTF8 = {0xEF, 0xBB, 0xBF};
+
+                if (byte1 == 0xFE && byte2 == 0xFF)//UnicodeBe
+                {
+                    targetEncoding = Encoding.BigEndianUnicode;
+                }
+
+                if (byte1 == 0xFF && byte2 == 0xFE && byte3 != 0xFF)//Unicode
+                {
+                    targetEncoding = Encoding.Unicode;
+                }
+
+                if (byte1 == 0xEF && byte2 == 0xBB && byte3 == 0xBF)//UTF8
+                {
+                    targetEncoding = Encoding.UTF8;
+                }
+
+                //恢复Seek位置
+                stream.Seek(origPos, SeekOrigin.Begin);
+            }
+            return targetEncoding;
+        }
+    }
+}
